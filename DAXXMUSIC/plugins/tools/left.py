f0c3252c09bc2e19b6aaefb2e13110dcc91ffc79
ARNAV SINGH <116391676+Xdarnav@users.noreply.github.com>
2024-04-07 15:45:02 +0530

    Update left.py

--- DAXXMUSIC/plugins/tools/left.py
+++ DAXXMUSIC/plugins/tools/left.py
@@ -6,7 +6,7 @@ from datetime import datetime
 
 @app.on_message(filters.left_chat_member)
 async def member_has_left(_, m: Message):
-    left_gif = "https://telegra.ph/file/d28047520fad932521368.mp4"
+    left_gif = "https://graph.org/file/7799fca07c686a7724a39.mp4"
     await m.reply_animation(
         animation=left_gif,
         caption=f"๏ sᴀᴅ ᴛᴏ sᴇᴇ ʏᴏᴜ ʟᴇᴀᴠɪɴɢ {m.from_user.mention}\n๏ ᴛᴀᴋᴇ ᴄᴀʀᴇ.!"
